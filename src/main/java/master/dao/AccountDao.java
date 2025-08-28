4ed8cbbf6687b92ec2bf65acdce0d9baa807b84d
MrinmoyAdhikary <mrinmoy.adh@gmail.com>
2025-08-28 08:32:24 +0530

    Update AccountDao.java

--- src/main/java/master/dao/AccountDao.java
+++ src/main/java/master/dao/AccountDao.java
@@ -1,54 +1,34 @@
 package master.dao;
+import java.sql.*;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import master.dto.AccountDto;
 import master.dto.CustomerDto;
 import master.utilities.ConnectionFactory;
 
-public class AccountDao {
-    private Connection cn = null;
-    private PreparedStatement ps = null;
-
-
-    private String insert_sql = "INSERT INTO account VALUES (?,?,?,?,?)";
-  
-    private String update_sql="update account set balance=?  where accno=?";
-
-
-    public void insertData(AccountDto adto) {
-               try {
-            ConnectionFactory con = new ConnectionFactory();
-            cn = con.getConn();
-
-            ps = cn.prepareStatement(insert_sql);
-            ps.setInt(1, adto.getAccno());
-            ps.setDate(2, adto.getOpendate());
-            ps.setDouble(3, adto.getBalance());
-            ps.setString(4, adto.getAccounttype());
-            ps.setString(5, adto.getCid());
-
-            ps.executeUpdate();
-            
-        } catch (SQLException se) {
-            se.printStackTrace();
-                       }
-        
-    }
-    public void updateData(AccountDto adto)
+public class CustomerDao {
+	private Connection cn=null;
+	private Statement st=null;
+	private PreparedStatement ps=null;
+	private ResultSet rs=null;
+	private String insert_sql="insert into customer values(?,?,?,?,?,?)";
+	private String update_sql="update customer set cemail=? ,cphno=? where cid=?";
+	private String delete_sql="delete from customer where cid=?";
+	
+	public void insertData(CustomerDto cdto)
 	{
 		try
 		{
 		
 			ConnectionFactory con=new ConnectionFactory();
 			cn=con.getConn();
-			ps=cn.prepareStatement(update_sql);
-		 ps.setDouble(1,adto.getBalance());
-		ps.setInt(2,adto.getAccno());
-		ps.executeUpdate();
+			ps=cn.prepareStatement(insert_sql);
+		    ps.setString(1,cdto.getCid());
+		    ps.setString(2,cdto.getCname());
+		    ps.setString(3,cdto.getCphno());
+		    ps.setString(4,cdto.getCemail());
+		    ps.setString(5,cdto.getCpanno());
+		    ps.setString(6,cdto.getCadhar());
+	
+		 ps.executeUpdate();
 		}
 		
 	
@@ -57,6 +37,53 @@ public class AccountDao {
 			se.printStackTrace();
 		}
 	}
- 
+	public boolean updateData(CustomerDto cdto)
+	{
+		try
+		{
+		
+			ConnectionFactory con=new ConnectionFactory();
+			cn=con.getConn();
+			ps=cn.prepareStatement(update_sql);
+		 ps.setString(2,cdto.getCphno());
+		ps.setString(1,cdto.getCemail());
+		 ps.setString(3, cdto.getCid());  
+		int row= ps.executeUpdate();
+		if(row>0)
+			return true;
+		}
+		
+	
+		catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+		return false;
+	}
+	public boolean deleteData(CustomerDto cdto)
+	{
+		try
+		{
+		
+			ConnectionFactory con=new ConnectionFactory();
+			cn=con.getConn();
+			ps=cn.prepareStatement(delete_sql);
+		 ps.setString(1,cdto.getCid());
+		
+		int row= ps.executeUpdate();
+		if(row>0)
+			return true;
+		}
+		
+	
+		catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+		return false;
+	}
+
 }
+
+
 
